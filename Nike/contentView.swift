81118c972a25778e2f1f90544f6a1c3296661f3e
SAY-Hong <bobseahee@gmail.com>
2023-12-05 23:49:49 +0900

    nikeShoesView
    - topIcon

---
+++ Nike/contentView.swift
@@ -0,0 +1,20 @@
+//
+//  detailView.swift
+//  DrawView
+//
+//  Created by 홍세희 on 2023/12/05.
+//
+
+import SwiftUI
+
+struct contentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    detailView()
+}
---
+++ Nike/topItem.swift
@@ -0,0 +1,18 @@
+//
+//  topItem.swift
+//  DrawView
+//
+//  Created by 홍세희 on 2023/12/05.
+//
+
+import SwiftUI
+
+struct topItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    topItem()
+}
